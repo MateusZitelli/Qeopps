b6ac98a4adef8b7a099499fa7975f4f864bdd16b
Mateus Zitelli <zitellimateus@gmail.com>
2012-10-03 21:55:54 -0300

    Global location added

--- fitness/client.scm
+++ fitness/client.scm
@@ -1,4 +1,4 @@
 (declare (uses tcp))
 (define-values (i o) (tcp-connect "localhost" 4242))
-(write-line "/home/mateus/dev/projeto/server\tteste.c\t-g -pthread -pg" o)
+(write-line "./\tteste.c\t-g -pthread -pg" o)
 (print (read-line i))
